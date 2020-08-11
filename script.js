546db9adfe80cdfdffd16eaff2266e0033cff02f
Cole Joko <joko.cole.cj@gmail.com>
2020-08-11 13:53:07 -0600

    debugging and added local storage

---
+++ script.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+
+  var today = moment().format('LLLL');
+  $("#currentDay").text(today);
+
+  var curTime = moment().format('H');
+
+  var workHours = [
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18
+  ];
+
+  for (var i = 0; i < workHours.length; i++) {
+    var timeBlock = moment().hours(workHours[i]).format('h A');
+    $("#hour-block")
+      .append(
+        `<div class="row hour" id="${workHours[i]}">
+          <div class="col col-sm-1 time-block"> ${timeBlock} </div>
+          <textarea class="col col-lg-9" id="notes"></textarea>
+          <button class="col col-sm-2 btn btn-block far fa-calendar-check saveBtn btn-lg">  SAVE  </button>
+        </div>`);
+  }
+
+  $(".hour").each(function () {
+    var holdTime = parseInt($(this).attr("id"))
+    curTime = parseInt(curTime);
+    if (holdTime > curTime) {
+      $(this).addClass("future");
+    }
+    else if (holdTime === curTime) {
+      $(this).addClass("present");
+    }
+    else $(this).addClass("past");
+  });
+
+  
+  $(".saveBtn").on("click", function(){
+    var myNotes = $(this).siblings("#notes").val();
+    var storeTime = $(this).parent().attr("id");
+    console.log(myNotes);
+    console.log(storeTime);
+    localStorage.setItem(storeTime, myNotes);
+  })
+
+  for (var j = 0; j < workHours.length; j++) {
+    var counter = j + 8;
+  $("#" + counter).children("#notes").val(localStorage.getItem(counter));
+  }
+
+});
